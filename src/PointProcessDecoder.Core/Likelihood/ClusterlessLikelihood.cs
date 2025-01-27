af8b6805b5ca912aba3bbdb74015f1b103bfa215
ncguilbeault <ncguilbeault@gmail.com>
2025-01-27 17:19:22 +0000

    Updated likelihood to use ignoreNoSpikes correctly with tensor

--- src/PointProcessDecoder.Core/Likelihood/ClusterlessLikelihood.cs
+++ src/PointProcessDecoder.Core/Likelihood/ClusterlessLikelihood.cs
@@ -4,8 +4,17 @@ namespace PointProcessDecoder.Core.Likelihood;
 
 public class ClusterlessLikelihood(bool ignoreNoSpikes = false) : ILikelihood
 {
-    private readonly bool _ignoreNoSpikes = ignoreNoSpikes;
-    public bool IgnoreNoSpikes => _ignoreNoSpikes;
+    private bool _ignoreNoSpikes = ignoreNoSpikes;
+    private Tensor _noSpikeLikelihood = ignoreNoSpikes ? zeros(1) : ones(1);
+    public bool IgnoreNoSpikes
+    {
+        get => _ignoreNoSpikes;
+        set 
+        {
+            _ignoreNoSpikes = value;
+            _noSpikeLikelihood = _ignoreNoSpikes ? zeros(1) : ones(1);
+        }
+    }
 
     public LikelihoodType LikelihoodType => LikelihoodType.Clusterless;
 
@@ -17,7 +26,7 @@ public class ClusterlessLikelihood(bool ignoreNoSpikes = false) : ILikelihood
         using var _ = NewDisposeScope();
         var channelConditionalIntensities = conditionalIntensities.ElementAt(0);
         var markConditionalIntensities = conditionalIntensities.ElementAt(1);
-        var logLikelihood = markConditionalIntensities.sum(dim: 0) - channelConditionalIntensities.sum(dim: 0) * _ignoreNoSpikes;
+        var logLikelihood = markConditionalIntensities.sum(dim: 0) - channelConditionalIntensities.sum(dim: 0) * _noSpikeLikelihood;
         logLikelihood -= logLikelihood.max(dim: -1, keepdim: true)
             .values;
         return logLikelihood
