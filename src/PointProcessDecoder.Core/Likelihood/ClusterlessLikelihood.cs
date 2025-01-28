bb61637d7040344d1e78624f6a5baa959e7bca8b
ncguilbeault <ncguilbeault@gmail.com>
2025-01-28 10:13:46 +0000

    Removed call to nan_to_num in clusterless likelihood calculation

--- src/PointProcessDecoder.Core/Likelihood/ClusterlessLikelihood.cs
+++ src/PointProcessDecoder.Core/Likelihood/ClusterlessLikelihood.cs
@@ -61,8 +61,7 @@ public class ClusterlessLikelihood : ILikelihood
         using var _ = NewDisposeScope();
         var channelConditionalIntensities = conditionalIntensities.ElementAt(0);
         var markConditionalIntensities = conditionalIntensities.ElementAt(1);
-        var logLikelihood = (markConditionalIntensities.sum(dim: 0) - channelConditionalIntensities.sum(dim: 0) * _noSpikeLikelihood)
-            .nan_to_num();
+        var logLikelihood = markConditionalIntensities.sum(dim: 0) - channelConditionalIntensities.sum(dim: 0) * _noSpikeLikelihood;
         logLikelihood -= logLikelihood.max(dim: -1, keepdim: true)
             .values;
         return logLikelihood
