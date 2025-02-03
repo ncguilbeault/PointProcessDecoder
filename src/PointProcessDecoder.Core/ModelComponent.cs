2d8c213e34c236ca1e8870224af391f0bd8d2f7d
ncguilbeault <ncguilbeault@gmail.com>
2025-02-03 14:54:09 +0000

    Added model component abstract class for virtual save and load functions

--- src/PointProcessDecoder.Core/ModelComponent.cs
+++ src/PointProcessDecoder.Core/ModelComponent.cs
@@ -3,30 +3,35 @@ using static TorchSharp.torch;
 namespace PointProcessDecoder.Core;
 
 /// <summary>
-/// Represents a single component of the model.
+/// Base abstract class for all model components.
 /// </summary>
-public interface IModelComponent : IDisposable
+public abstract class ModelComponent : IModelComponent
 {
     /// <summary>
-    /// The device on which the model component in located.
+    /// The device on which the model component is stored.
     /// </summary>
-    public Device Device { get; }
+    public abstract Device Device { get; }
 
     /// <summary>
     /// The scalar type of the model component.
     /// </summary>
-    public ScalarType ScalarType { get; }
+    public abstract ScalarType ScalarType { get; }
 
     /// <summary>
     /// Saves the state of the model component using the specified path.
     /// </summary>
     /// <param name="basePath"></param>
-    public void Save(string basePath);
+    public virtual void Save(string basePath) { }
 
     /// <summary>
     /// Loads the state of the model component using the specified path.
     /// </summary>
     /// <param name="basePath"></param>
     /// <returns></returns>
-    public IModelComponent Load(string basePath);
+    public virtual IModelComponent Load(string basePath) { return this; }
+
+    /// <summary>
+    /// Disposes of the model component.
+    /// </summary>
+    public virtual void Dispose() { }
 }
