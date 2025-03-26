c9d5b0d091553f1d9fdbe9b5ad66cfa2c1da7e47
ncguilbeault <ncguilbeault@gmail.com>
2025-03-26 18:21:23 +0000

    Add `ReplayClassifier` class and `ClassifierType` enum for a new type of model classification

---
+++ src/PointProcessDecoder.Core/Classifier/ClassifierType.cs
@@ -0,0 +1,12 @@
+namespace PointProcessDecoder.Core.Classifier;
+
+/// <summary>
+/// Represents the classifier type of the model.
+/// </summary>
+public enum ClassifierType
+{
+    /// <summary>
+    /// Represents a replay classifier.
+    /// </summary>
+    ReplayClassifier
+}
---
+++ src/PointProcessDecoder.Core/Classifier/ReplayClassifier.cs
@@ -0,0 +1,138 @@
+using static TorchSharp.torch;
+
+using PointProcessDecoder.Core.Transitions;
+using TorchSharp;
+
+namespace PointProcessDecoder.Core.Classifier;
+
+public class ReplayClassifier : ModelComponent, IClassifier
+{
+    private readonly Device _device;
+    /// <inheritdoc/>
+    public override Device Device => _device;
+
+    private readonly ScalarType _scalarType;
+    /// <inheritdoc/>
+    public override ScalarType ScalarType => _scalarType;
+
+    public ClassifierType ClassifierType => ClassifierType.ReplayClassifier;
+
+    private const int NUM_STATES = 2;
+
+    /// <summary>
+    /// A 2x2 transition matrix for discrete states.
+    /// </summary>
+    private Tensor _discreteTransitions;
+
+    /// <summary>
+    /// The continuous transitions for each state, shape [2, n, n].
+    /// </summary>
+    private Tensor _continuousTransitions;
+
+    /// <summary>
+    /// The user-defined continuous state space.
+    /// </summary>
+    private readonly IStateSpace _stateSpace;
+
+    private Tensor _posterior;
+    /// <summary>
+    /// The posterior distribution over the latent space.
+    /// </summary>
+    public Tensor Posterior => _posterior;
+
+    private readonly Tensor _initialState = empty(0);
+    /// <inheritdoc/>
+    public Tensor InitialState => _initialState;
+
+    private readonly double _eps;
+    private readonly long[] _posteriorShape;
+
+    public ReplayClassifier(
+        IStateSpace stateSpace,
+        double? sigmaRandomWalk = null,
+        double stayProbability = 0.99,
+        Device? device = null,
+        ScalarType? scalarType = null
+    )
+    {
+        _device = device ?? CPU;
+        _scalarType = scalarType ?? ScalarType.Float32;
+        _eps = finfo(_scalarType).eps;
+        _stateSpace = stateSpace;
+
+        _discreteTransitions = tensor(
+            new double[] 
+            { 
+                stayProbability, 
+                1 - stayProbability,
+                1 - stayProbability,
+                stayProbability 
+            },
+            dtype: _scalarType,
+            device: _device
+        ).reshape(NUM_STATES, NUM_STATES);
+
+        var randomWalkTransitions = new RandomWalkTransitions(
+            stateSpace: _stateSpace, 
+            sigma: sigmaRandomWalk,
+            device: _device,
+            scalarType: _scalarType
+        );
+
+        var uniformTransitions = new UniformTransitions(
+            stateSpace: _stateSpace,
+            device: _device,
+            scalarType: _scalarType
+        );
+
+        var n = _stateSpace.Points.size(0);
+
+        _continuousTransitions = cat([
+            uniformTransitions.Transitions.unsqueeze(0),
+            uniformTransitions.Transitions.unsqueeze(0),
+            uniformTransitions.Transitions.unsqueeze(0),
+            randomWalkTransitions.Transitions.unsqueeze(0)
+        ], dim: 0).reshape([NUM_STATES, n * NUM_STATES, n]);
+        
+        _initialState = ones([NUM_STATES, n], dtype: _scalarType, device: _device) / n;
+        _posterior = empty(0);
+        _posteriorShape = [.. new long[] { NUM_STATES }.Concat(_stateSpace.Shape)];
+    }
+
+    /// <inheritdoc/>
+    public Tensor Decode(Tensor likelihood)
+    {
+        using var _ = NewDisposeScope();
+
+        var outputShape = new long[] { likelihood.size(0) }
+            .Concat(_posteriorShape);
+
+        var output = zeros([.. outputShape], dtype: _scalarType, device: _device);
+
+        var startIndex = 0;
+
+        if (_posterior.NumberOfElements == 0) {
+            _posterior = (_initialState * likelihood[0])
+                .nan_to_num()
+                .clamp_min(_eps);
+            _posterior /= _posterior.sum();
+            startIndex++;
+        }
+
+        for (int i = startIndex; i < likelihood.size(0); i++)
+        {
+            var discretePred = _discreteTransitions.matmul(_posterior);
+            var discretePredReshaped = discretePred.flatten();
+            var discretePredExpanded = discretePredReshaped.reshape(1, -1, 1);
+            var product = _continuousTransitions * discretePredExpanded;
+            var newPosterior = product.sum(dim: 1);
+            _posterior = (newPosterior * likelihood[i])
+                .nan_to_num()
+                .clamp_min(_eps);
+            _posterior /= _posterior.sum();
+            output[i] = _posterior.reshape(_posteriorShape);
+        }
+        _posterior.MoveToOuterDisposeScope();
+        return output.MoveToOuterDisposeScope();
+    }
+}
